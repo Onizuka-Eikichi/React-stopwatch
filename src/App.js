593ea54febc81f44908feae36b0c6031934957e1
Victor Araujo <victoraraujo@pop-os.localdomain>
2021-03-26 17:16:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import cronometro from './assets/cronometro.png';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      number: 0
+    };
+    this.timer = null;
+    this.start = this.start.bind(this);
+    this.clear = this.clear.bind(this);
+
+  }
+
+  start(){
+
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+
+    }else{
+      this.timer = setInterval(()=>{
+      let state = this.state;
+      state.number += 0.01;
+      this.setState(state);
+    },10);}
+    
+  }
+
+  clear(){}
+
+
+  render(){
+    return(
+
+      <div className="container">
+
+          <img src={cronometro} alt="stopwatch"/>
+          <span className="timer">{this.state.number.toFixed(2)}</span>
+
+            <div className="areaBtn">
+                <span className="btn" onClick={this.start}>Start</span>
+                <span className="btn" onClick={this.clear}>Clear</span>
+            </div>
+      </div>
+
+    );
+  }
+
+
 }
-
-export default App;
